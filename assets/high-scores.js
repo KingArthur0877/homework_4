8ff77e071b617eec0bffd9fd5f13dbc6fda2ae99
Adam Arthur <adamarthur44@gmail.com>
2021-03-28 16:04:41 +0100

    added JS

--- assets/high-scores.js
+++ assets/high-scores.js
@@ -1,22 +1,103 @@
-var highScore = document.querySelector("#highScore");
-var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");
+const bodyElement = document.body;
+const goBackBtn = document.getElementById("go-back");
+const clearBtn = document.getElementById("clear");
+// Get scores from local storage and sort into order.
+const getFromLocalStorage = () => {
+  const highScores = localStorage.getItem("highScores");
+  const highScoresArray = JSON.parse(highScores);
+  if (highScores) {
+    // Sort the high scores into a new array...
+    const sortFunction = (firstEl, secondEl) => {
+      const firstElValue = firstEl[1];
+      const secondElValue = secondEl[1];
 
-clear.addEventListner("click", function () {
-    localStorage.clear();
-    location.reload();
-});
+      return secondElValue - firstElValue;
+    };
+    highScoresArray.sort(sortFunction);
+    return highScoresArray;
+  } else {
+    return [];
+  }
+};
+// function to render the High Scores
+const renderHighScoresTable = (highScores) => {
+  if (highScores.length === 0) {
+    const resultsEmpty = noResults();
+    bodyElement.appendChild(resultsEmpty);
+  } else {
+    const showResults = resultsTable(highScores);
+    bodyElement.appendChild(showResults);
+  }
+};
+// Creates the high scores table if scores in local storage.
+const resultsTable = (highScores) => {
+  const tableDiv = document.createElement("div");
+  tableDiv.setAttribute("id", "table");
 
-var allScores = localStorage.getItem("allScores");
-allScores = JSON.parse(allScores)
+  const resultList = document.createElement("ul");
+  resultList.setAttribute("id", "results-list");
 
-if (allScores !== null) {
-    for (var i = 0; i < allScores.length; i++) {
-        var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initials + "" + allScores[i].score;
-        highScore.appendChild(createLi);
-    }
-}
-goBack.addEventListener("click", function () {
-    window.location.replace("./index.html");
-});
+  highScores.forEach(function (highScores) {
+    const listItem = document.createElement("li");
+    listItem.setAttribute("id", "list-item");
+    listItem.append(highScores);
+    resultList.append(listItem);
+    return highScores;
+  });
+
+  const goBackBtn = document.createElement("button");
+  goBackBtn.setAttribute("id", "go-back");
+  goBackBtn.addEventListener("click", goBack);
+  goBackBtn.textContent = "Go Back";
+
+  const clearBtn = document.createElement("button");
+  clearBtn.setAttribute("id", "clear");
+  clearBtn.addEventListener("click", clear);
+  clearBtn.textContent = "Clear";
+
+  tableDiv.append(resultList, goBackBtn, clearBtn);
+
+  return tableDiv;
+};
+// Shows a 'No Results' message when local storage is empty.
+const noResults = () => {
+  const noResultsDiv = document.createElement("div");
+  noResultsDiv.setAttribute("id", "results-div");
+
+  const noResultsH2 = document.createElement("h2");
+  noResultsH2.textContent = "No results to show.";
+
+  const goBackBtn = document.createElement("button");
+  goBackBtn.setAttribute("id", "go-back");
+  goBackBtn.addEventListener("click", goBack);
+  goBackBtn.textContent = "Go Back";
+
+  const clearBtn = document.createElement("button");
+  clearBtn.setAttribute("id", "clear");
+  clearBtn.addEventListener("click", clear);
+  clearBtn.textContent = "Clear";
+
+  noResultsDiv.append(noResultsH2, goBackBtn, clearBtn);
+
+  return noResultsDiv;
+};
+// Loads information when page loads.
+const onLoad = () => {
+  const highScores = getFromLocalStorage();
+  renderHighScoresTable(highScores);
+};
+// Goes back to main page.
+const goBack = () => {
+  location.href = "https://KingArthur0877.github.io/homework_4/index.html";
+};
+// Clears the local storage & removes table if present.
+const clear = () => {
+  localStorage.clear();
+  const resultsTable = document.getElementById("table");
+  bodyElement.removeChild(resultsTable);
+
+  const noResult = noResults();
+  bodyElement.appendChild(noResult);
+};
+
+window.addEventListener("load", onLoad);
--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,10 @@ const welcomeMain = document.getElementById("welcome")
 const timer = document.getElementById("timer")
 const questionOne = [
     "What header do you start a with?",
-    "h1",
-    "h2",
     "h3",
+    "h1",
     "h4",
+    "h2",
 ];
 const questionTwo = [
     "What does HTML stand for?",
@@ -20,8 +20,8 @@ const questionThree = [
     "What was the first coding language?",
     "HTML",
     "CSS",
-    "FORTRAN",
     "Python",
+    "FORTRAN",
 ]
 const questionFour = [
     "What is the basic setup for a HTML?",
@@ -77,6 +77,192 @@ const startTimer = () => {
     const timer = setInterval(countdown, 1000);
 };
 
-const contructQuestion = () => {
+const constructQuestion = () => {
+    const questionCardDiv = document.createElement("div");
+    questionCardDiv.setAttribute("id", "questions");
+
+    const h2Element = document.createElement("h2")
+    h2Element.textContent = questionOne[0];
+
+    const btnElement1 = document.createElement("button");
+    btnElement1.setAttribute("id", "Answer-One")
+    btnElement1.textContent = questionOne[1];
+    const btnElement2 = document.createElement("button");
+    btnElement2.setAttribute("id", "Answer-Two");
+    btnElement2.textContent = questionOne[2];
+    const btnElement3 = document.createElement("button");
+    btnElement3.setAttribute("id", "Answer-Three");
+    btnElement3.textContent = questionOne[3];
+    const btnElement4 = document.createElement("button");
+    btnElement4.setAttribute("id", "Answer-Four");
+    btnElement4.textContent = questionOne[4];
+
+    questionCardDiv.append(
+        h2Element,
+        btnElement1,
+        btnElement2,
+        btnElement3,
+        btnElement4,
+    );
+
+    questionValue += 1;
+       const Answer1 = () => {
+        h2Element.textContent = questionTwo[0];
+        btnElement1.textContent = questionTwo[1];
+        btnElement2.textContent = questionTwo[2];
+        btnElement3.textContent = questionTwo[3];
+        btnElement4.textContent = questionTwo[4];
     
-}
+        btnElement2.removeEventListener("click", incorrect);
+        btnElement3.removeEventListener("click", Answer1);
+    
+        btnElement2.addEventListener("click", Answer1);
+        btnElement3.addEventListener("click", incorrect);
+        questionValue += 1;
+       };
+
+       const Answer2 = () => {
+        h2Element.textContent = questionThree[0];
+        btnElement1.textContent = questionThree[1];
+        btnElement2.textContent = questionThree[2];
+        btnElement3.textContent = questionThree[3];
+        btnElement4.textContent = questionThree[4];
+    
+        btnElement2.removeEventListener("click", Answer2);
+        btnElement4.removeEventListener("click", incorrect);
+    
+        btnElement2.addEventListener("click", incorrect);
+        btnElement4.addEventListener("click", Answer3);
+        questionValue += 1;
+      };
+
+      const Answer3 = () => {
+        h2Element.textContent = questionFour[0];
+        btnElement1.textContent = questionFour[1];
+        btnElement2.textContent = questionFour[2];
+        btnElement3.textContent = questionFour[3];
+        btnElement4.textContent = questionFour[4];
+    
+        btnElement1.removeEventListener("click", incorrect);
+        btnElement4.removeEventListener("click", Answer3);
+    
+        btnElement1.addEventListener("click", Answer4);
+        btnElement4.addEventListener("click", incorrect);
+        questionValue += 1;
+      };
+
+      const Answer4 = () => {
+        h2Element.textContent = questionFive[0];
+        btnElement1.textContent = questionFive[1];
+        btnElement2.textContent = questionFive[2];
+        btnElement3.textContent = questionFive[3];
+        btnElement4.textContent = questionFive[4];
+    
+        btnElement1.removeEventListener("click", Answer4);
+        btnElement2.removeEventListener("click", incorrect);
+    
+        btnElement1.addEventListener("click", incorrect);
+        btnElement2.addEventListener("click", Answer5);
+        questionValue += 1;
+      };   
+      const Answer5 = () => {
+        const questionCardID = document.getElementById("question-card");
+        bodyElement.removeChild(questionCardID);
+    
+        const endGameContainer = endGameCard();
+        bodyElement.appendChild(endGameContainer);
+        questionValue += 1;
+      };
+      
+      const incorrect = () => {
+        timerValue -= 10;
+        if (timerValue === 0) {
+          clearInterval(timer);
+        }
+        if (incorrect) {
+          alert("Incorrect! Guess again!");
+        }
+      };
+
+    btnElement1.addEventListener("click", incorrect);
+    btnElement2.addEventListener("click", incorrect);
+    btnElement3.addEventListener("click", Answer1);
+    btnElement4.addEventListener("click", incorrect);
+
+    return questionCardDiv;
+};
+const endGameCard = () => {
+    const endGameDiv = document.createElement("div");
+    endGameDiv.setAttribute("id", "end-game");
+  
+    const h2EndGame = document.createElement("h2");
+    h2EndGame.textContent = "Congratulations!";
+  
+    const enterName = document.createElement("input");
+    enterName.setAttribute("id", "player-name");
+    enterName.setAttribute("placeholder", "Enter your Name");
+  
+    const lineBreak = document.createElement("p");
+  
+    const submitBtn = document.createElement("button");
+    submitBtn.setAttribute("id", "submit-btn");
+    submitBtn.textContent = "Submit Score";
+    submitBtn.addEventListener("click", submitScore);
+  
+    endGameDiv.append(h2EndGame, enterName, lineBreak, submitBtn);
+  
+    return endGameDiv;
+  };
+    
+let submitScore = (event) => {
+    event.preventDefault();
+    const name = document.querySelector("#player-name").value;
+    const score = timerValue;
+  
+    const finalScore = [name, score];
+  
+    const highScore = getHighScores();
+    highScore.push(finalScore);
+    localStorage.setItem("highScores", JSON.stringify(highScore));
+  
+    location.href ="https://KingArthur0877.github.io/homework_4/high-score.html"
+    };    
+
+const constructGameOver = () => {
+        const gameOverDiv = document.createElement("div");
+        gameOverDiv.setAttribute("id", "game-over");
+      
+        const h2GameOverElement = document.createElement("h2");
+        h2GameOverElement.textContent = "GAME OVER";
+      
+        const btnElementTryAgain = document.createElement("button");
+        btnElementTryAgain.setAttribute("id", "try-again");
+        btnElementTryAgain.textContent = "Try Again";
+      
+        gameOverDiv.append(h2GameOverElement, btnElementTryAgain);
+        btnElementTryAgain.addEventListener("click", tryAgain);
+      
+        return gameOverDiv;
+      };
+
+const startGame = () => {
+
+    bodyElement.removeChild(introMain);
+ 
+    startTimer();
+  
+  
+    const questionsDiv = constructQuestionCard();
+    bodyElement.appendChild(questionsDiv);
+  };
+  
+  const tryAgain = () => {
+    const gameOverID = document.getElementById("game-over");
+  
+    bodyElement.removeChild(gameOverID);
+    bodyElement.appendChild(introMain);
+    timerValue = 60;
+    questionValue = 0;
+  };
+
+  startBtnElement.addEventListener("click", startGame);
